aec633ee0e2c724528999b967e4bf61314df86e2
ApplianceJohn <thednbgremlin@gmail.com>
2022-03-04 20:56:36 -0500

    Fixed marked.js not respecting new lines

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,11 @@ import Card from './Card';
 import Editor from './Editor';
 import Preview from './Preview';
 
+//Support carriage returns
+marked.setOptions({
+	breaks: true
+});
+
 export default function App(props) {
   const init = `# Minimal Markdown Editor\n## A React Applet for freeCodeCamp\n![freecodecamp](https://design-style-guide.freecodecamp.org/downloads/fcc_secondary_small.svg)\n\nThis applet uses \`marked.js\` to parse content.\nTo include it in your own work, use:\n\`\`\`\nimport { marked } from "https://cdn.skypack.dev/marked@latest";\n//Support carriage returns\nmarked.setOptions({\n	breaks: true\n});\n\`\`\`\nIt's important to note that Marked **does not sanitize!** You need to use an external plugin or library, such as [DOMPurify](https://github.com/cure53/DOMPurify):\n> DOMPurify - a DOM-only, super-fast, uber-tolerant XSS sanitizer for HTML, MathML and SVG. DOMPurify works with a secure default, but offers a lot of configurability and hooks.\n### Find me online at:\n- [Twitter](https://twitter.com/rinsethewax)\n- [Github](https://github.com/ApplianceJohn)`;
 
